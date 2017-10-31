0cd67e7661828ade60d0acf169024a6073584c46
Ken Li <iliawnek@gmail.com>
2017-10-31 23:49:14 +0000

    Close #1: Set up Firebase and VueFire bindings

---
+++ firebase/firebase.js
@@ -0,0 +1,14 @@
+import Firebase from 'firebase'
+
+const config = {
+  apiKey: 'AIzaSyBxeua6P2GlAONRY2rGAqQK8zWbDuE-Pl8',
+  authDomain: 'disaster-damage-video-tagger.firebaseapp.com',
+  databaseURL: 'https://disaster-damage-video-tagger.firebaseio.com',
+  projectId: 'disaster-damage-video-tagger',
+  storageBucket: 'disaster-damage-video-tagger.appspot.com',
+  messagingSenderId: '737684330599',
+}
+
+const app = Firebase.initializeApp(config)
+
+export const db = Firebase.database()
