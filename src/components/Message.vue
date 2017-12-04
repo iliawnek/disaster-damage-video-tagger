a95e09b15629035aa7ae260ca6eb89ef63bc3813
Ken Li <iliawnek@gmail.com>
2017-12-04 18:32:56 +0000

    Create Message component for displaying messages

---
+++ src/components/Message.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: 'message',
+
+  props: {
+    message: String,
+    icon: String,
+  },
+}
+</script>
+
+<template lang="pug">
+  md-content.md-primary
+    md-icon {{icon}}
+    span#message {{message}}
+</template>
+
+<style scoped lang="sass">
+  .md-content
+    padding: 24px
+  .md-icon
+    margin-right: 16px
+  #message
+    font-weight: bold
+</style>
