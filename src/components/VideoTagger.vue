b3d9c2e83a8c8bf85220bc272a40773d89dbd07a
Ken Li <iliawnek@gmail.com>
2017-12-14 17:37:27 +0000

    Apply custom theme to video-player, fixes #37

--- src/components/VideoTagger.vue
+++ src/components/VideoTagger.vue
@@ -56,7 +56,7 @@ export default {
       // create tag button
       const createTagButton = document.createElement('div')
       createTagButton.className = 'vjs-tagger-button vjs-create-tag-button'
-      createTagButton.innerText = 'Create tag'
+      createTagButton.innerText = 'Create a new tag'
       createTagButton.addEventListener('click', () => {
         this.startCrop()
       })
@@ -152,10 +152,70 @@ export default {
 
 <style lang="sass">
   @import '~cropperjs/dist/cropper.css'
+  $white: rgba(255,255,255,0.6)
+  $white-on-white: rgba(255,255,255,0.5)
+
+  .video-js:hover
+    .vjs-big-play-button
+      background-color: $white
+
+  .vjs-has-started.vjs-paused .vjs-big-play-button
+    display: block
+    transform: translate(-50%, calc(-50% - 50px))
 
   .video-js
+    .vjs-control-bar
+      height: 50px
+      background-color: $white
+      color: black
+
+    .vjs-big-play-button
+      position: absolute
+      left: 50%
+      top: 50%
+      transform: translate(-50%, -50%)
+      width: 80px
+      height: 80px
+      border-radius: 50%
+      border: none
+      background-color: $white
+      color: black
+      .vjs-icon-placeholder::before
+        font-size: 40px
+        line-height: 2em
+        padding-left: 3px
+    .vjs-big-play-button:hover
+      background-color: white
+
+    // play, fullscreen buttons
+    .vjs-button
+      width: 50px
+      font-size: 14px
+      background-color: white !important
+      // button icon position
+      .vjs-icon-placeholder::before
+         line-height: 2.1em
+
+    .vjs-progress-control
+      .vjs-slider
+        background-color: transparent
+        height: 100%
+        .vjs-slider-bar::before
+          display: none
+        .vjs-load-progress
+          display: none
+        .vjs-play-progress
+          background-color: $white-on-white
+
+      .vjs-progress-holder
+        margin: 0
+
+    .vjs-remaining-time
+      display: none
+
+    // custom buttons
     .vjs-tagger-button
-      background: rgba(255,255,255,0.8)
+      background: $white
       color: black
       font-family: Roboto, sans-serif
       font-size: 16px
@@ -164,14 +224,16 @@ export default {
       user-select: none
       cursor: pointer
       padding: 16px
-      margin: 8px
+      line-height: 1em
+    .vjs-tagger-button:hover
+      background-color: white
 
     .vjs-create-tag-button
       display: none
       position: absolute
       top: 50%
       left: 50%
-      transform: translate(-50%, -50%)
+      transform: translate(-50%, calc(-50% + 40px))
 
     #vjs-canvas-container
       position: absolute
@@ -192,6 +254,8 @@ export default {
       left: 50%
       margin: 8px
       transform: translate(calc(-50% - 8px), calc(100% + 16px))
+      .vjs-tagger-button
+        margin: 8px
 
   .vjs-paused.vjs-has-started .vjs-create-tag-button
       display: block
