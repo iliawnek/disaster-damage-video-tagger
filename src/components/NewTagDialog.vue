cf981073e37a80bdfb86d665bc5eec8db5716ea5
Ken Li <iliawnek@gmail.com>
2018-01-03 22:14:22 +0000

    Fix #43, fix #44

--- src/components/NewTagDialog.vue
+++ src/components/NewTagDialog.vue
@@ -211,164 +211,163 @@ export default {
       .overlay-progress(v-if="uploading")
         md-progress-spinner(md-mode="indeterminate")
       md-dialog-title {{getDialogTitle}}
-      img(v-if="images" :src="images.cropped")
-      form(v-if="isCurrentStageDialog" novalidate @submit.prevent="submitForm")
-        md-dialog-content
-          md-field(:class="getValidationClass('type')")
-            label(for="type") Type
+      img.crop-image(v-if="images" :src="images.cropped")
+      md-dialog-content(v-if="isCurrentStageDialog")
+        md-field(:class="getValidationClass('type')")
+          label(for="type") Type
+          md-select(
+          v-model="form.type"
+          name="type"
+          md-dense
+          )
+            md-option(
+            v-for="type in types"
+            :key="type"
+            :value="type"
+            ) {{type}}
+          span.md-error(v-if="$v.form.type && !$v.form.type.required") Required.
+
+        div(v-if="form.type === 'people'")
+          md-field(:class="getValidationClass('people', 'amount')")
+            label(for="people-amount") Amount
             md-select(
-            v-model="form.type"
-            name="type"
+            v-model="form.people.amount"
+            name="people-amount"
             md-dense
             )
               md-option(
-              v-for="type in types"
-              :key="type"
-              :value="type"
-              ) {{type}}
-            span.md-error(v-if="$v.form.type && !$v.form.type.required") Required.
-
-          div(v-if="form.type === 'people'")
-            md-field(:class="getValidationClass('people', 'amount')")
-              label(for="people-amount") Amount
-              md-select(
-              v-model="form.people.amount"
-              name="people-amount"
-              md-dense
-              )
-                md-option(
-                v-for="option in people.amount"
-                :key="option"
-                :value="option"
-                ) {{option}}
-              span.md-error(v-if="$v.form.people && !$v.form.people.amount.required") Required.
-            md-field(:class="getValidationClass('people', 'state')")
-              label(for="people-state") State
-              md-select(
-              v-model="form.people.state"
-              name="people-state"
-              md-dense
-              )
-                md-option(
-                v-for="option in people.state"
-                :key="option"
-                :value="option"
-                ) {{option}}
-              span.md-error(v-if="$v.form.people && !$v.form.people.state.required") Required.
-            md-field(:class="getValidationClass('people', 'freedom')")
-              label(for="people-freedom") Freedom
-              md-select(
-              v-model="form.people.freedom"
-              name="people-freedom"
-              md-dense
-              )
-                md-option(
-                v-for="option in people.freedom"
-                :key="option"
-                :value="option"
-                ) {{option}}
-              span.md-error(v-if="$v.form.people && !$v.form.people.freedom.required") Required.
-
-          div(v-if="form.type === 'water'")
-            md-field(:class="getValidationClass('water', 'form')")
-              label(for="water-form") Form
-              md-select(
-              v-model="form.water.form"
-              name="water-form"
-              md-dense
-              )
-                md-option(
-                v-for="option in water.form"
-                :key="option"
-                :value="option"
-                ) {{option}}
-              span.md-error(v-if="$v.form.water && !$v.form.water.form.required") Required.
-
-          div(v-if="form.type === 'vehicle'")
-            md-field(:class="getValidationClass('vehicle', 'state')")
-              label(for="vehicle-state") State
-              md-select(
-              v-model="form.vehicle.state"
-              name="vehicle-state"
-              md-dense
-              )
-                md-option(
-                v-for="option in vehicle.state"
-                :key="option"
-                :value="option"
-                ) {{option}}
-              span.md-error(v-if="$v.form.vehicle && !$v.form.vehicle.state.required") Required.
-
-          div(v-if="form.type === 'area'")
-            md-field(:class="getValidationClass('area', 'form')")
-              label(for="area-form") Form
-              md-select(
-              v-model="form.area.form"
-              name="area-form"
-              md-dense
-              )
-                md-option(
-                v-for="option in area.form"
-                :key="option"
-                :value="option"
-                ) {{option}}
-              span.md-error(v-if="$v.form.area && !$v.form.area.form.required") Required.
-            md-field(:class="getValidationClass('area', 'shelter')")
-              label(for="area-shelter") Shelter
-              md-select(
-              v-model="form.area.shelter"
-              name="area-shelter"
-              md-dense
-              )
-                md-option(
-                v-for="option in area.shelter"
-                :key="option"
-                :value="option"
-                ) {{option}}
-              span.md-error(v-if="$v.form.area && !$v.form.area.shelter.required") Required.
-            md-field(:class="getValidationClass('area', 'service')")
-              label(for="area-service") Service
-              md-select(
-              v-model="form.area.service"
-              name="area-service"
-              md-dense
-              )
-                md-option(
-                v-for="option in area.service"
-                :key="option"
-                :value="option"
-                ) {{option}}
-              span.md-error(v-if="$v.form.area && !$v.form.area.service.required") Required.
-
-          div(v-if="form.type === 'damage'")
-            md-field(:class="getValidationClass('damage', 'form')")
-              label(for="damage-form") Form
-              md-select(
-              v-model="form.damage.form"
-              name="damage-form"
-              md-dense
-              )
-                md-option(
-                v-for="option in damage.form"
-                :key="option"
-                :value="option"
-                ) {{option}}
-              span.md-error(v-if="$v.form.damage && !$v.form.damage.form.required") Required.
-
-          md-field(:class="getValidationClass('description')")
-            label(for="description") Description
-            md-textarea(
-            v-model="form.description"
-            name="description"
-            md-autogrow
+              v-for="option in people.amount"
+              :key="option"
+              :value="option"
+              ) {{option}}
+            span.md-error(v-if="$v.form.people && !$v.form.people.amount.required") Required.
+          md-field(:class="getValidationClass('people', 'state')")
+            label(for="people-state") State
+            md-select(
+            v-model="form.people.state"
+            name="people-state"
+            md-dense
             )
-            span.md-error(v-if="$v.form.description && !$v.form.description.required") Required.
+              md-option(
+              v-for="option in people.state"
+              :key="option"
+              :value="option"
+              ) {{option}}
+            span.md-error(v-if="$v.form.people && !$v.form.people.state.required") Required.
+          md-field(:class="getValidationClass('people', 'freedom')")
+            label(for="people-freedom") Freedom
+            md-select(
+            v-model="form.people.freedom"
+            name="people-freedom"
+            md-dense
+            )
+              md-option(
+              v-for="option in people.freedom"
+              :key="option"
+              :value="option"
+              ) {{option}}
+            span.md-error(v-if="$v.form.people && !$v.form.people.freedom.required") Required.
 
-        md-dialog-actions
-          md-button.md-secondary(@click="previousStage") Back
-          md-button.md-primary(type="submit") Create
+        div(v-if="form.type === 'water'")
+          md-field(:class="getValidationClass('water', 'form')")
+            label(for="water-form") Form
+            md-select(
+            v-model="form.water.form"
+            name="water-form"
+            md-dense
+            )
+              md-option(
+              v-for="option in water.form"
+              :key="option"
+              :value="option"
+              ) {{option}}
+            span.md-error(v-if="$v.form.water && !$v.form.water.form.required") Required.
 
-      div(v-else-if="isCurrentStageDone")
+        div(v-if="form.type === 'vehicle'")
+          md-field(:class="getValidationClass('vehicle', 'state')")
+            label(for="vehicle-state") State
+            md-select(
+            v-model="form.vehicle.state"
+            name="vehicle-state"
+            md-dense
+            )
+              md-option(
+              v-for="option in vehicle.state"
+              :key="option"
+              :value="option"
+              ) {{option}}
+            span.md-error(v-if="$v.form.vehicle && !$v.form.vehicle.state.required") Required.
+
+        div(v-if="form.type === 'area'")
+          md-field(:class="getValidationClass('area', 'form')")
+            label(for="area-form") Form
+            md-select(
+            v-model="form.area.form"
+            name="area-form"
+            md-dense
+            )
+              md-option(
+              v-for="option in area.form"
+              :key="option"
+              :value="option"
+              ) {{option}}
+            span.md-error(v-if="$v.form.area && !$v.form.area.form.required") Required.
+          md-field(:class="getValidationClass('area', 'shelter')")
+            label(for="area-shelter") Shelter
+            md-select(
+            v-model="form.area.shelter"
+            name="area-shelter"
+            md-dense
+            )
+              md-option(
+              v-for="option in area.shelter"
+              :key="option"
+              :value="option"
+              ) {{option}}
+            span.md-error(v-if="$v.form.area && !$v.form.area.shelter.required") Required.
+          md-field(:class="getValidationClass('area', 'service')")
+            label(for="area-service") Service
+            md-select(
+            v-model="form.area.service"
+            name="area-service"
+            md-dense
+            )
+              md-option(
+              v-for="option in area.service"
+              :key="option"
+              :value="option"
+              ) {{option}}
+            span.md-error(v-if="$v.form.area && !$v.form.area.service.required") Required.
+
+        div(v-if="form.type === 'damage'")
+          md-field(:class="getValidationClass('damage', 'form')")
+            label(for="damage-form") Form
+            md-select(
+            v-model="form.damage.form"
+            name="damage-form"
+            md-dense
+            )
+              md-option(
+              v-for="option in damage.form"
+              :key="option"
+              :value="option"
+              ) {{option}}
+            span.md-error(v-if="$v.form.damage && !$v.form.damage.form.required") Required.
+
+        md-field(:class="getValidationClass('description')")
+          label(for="description") Description
+          md-textarea(
+          v-model="form.description"
+          name="description"
+          md-autogrow
+          )
+          span.md-error(v-if="$v.form.description && !$v.form.description.required") Required.
+
+      md-dialog-actions(v-if="isCurrentStageDialog")
+        md-button.md-secondary(@click="previousStage") Back
+        md-button.md-primary(@click="submitForm") Create
+
+      div(v-if="isCurrentStageDone")
         md-dialog-content This tag has been successfully created!
         md-dialog-actions
           md-button.md-primary(@click="nextStage") Done
@@ -387,4 +386,9 @@ export default {
     display: flex
     justify-content: center
     align-items: center
+  .crop-image
+    object-fit: contain
+    background-color: black
+    margin-bottom: 16px
+    max-height: 20vh
 </style>
