0fd2bbc0cbd2be74be92151ff22611174bce393d
Ken Li <iliawnek@gmail.com>
2018-01-09 17:18:26 +0000

    Display all information on tag-card

--- src/components/TagCard.vue
+++ src/components/TagCard.vue
@@ -2,27 +2,60 @@
 export default {
   name: 'tag-card',
 
+  data () {
+    return {
+      zoomedIn: false,
+    }
+  },
+
   props: [
     'tag',
   ],
+
+  computed: {
+    imageSrc () {
+      const {highlighted, cropped} = this.tag.crop.images
+      return this.zoomedIn ? cropped : highlighted
+    },
+  },
+
+  methods: {
+    toggleZoom () {
+      this.zoomedIn = !this.zoomedIn
+    },
+  },
 }
 </script>
 
 <template lang="pug">
   md-card.tag-card
-    md-card-media(md-ratio="1:1").media
-      img.image(
-      :src="tag.crop.images.highlighted"
-      )
+    img.image(
+    :src="imageSrc"
+    @click="toggleZoom"
+    )
+    .text
+      ul
+        li
+          b details:&nbsp;
+          | {{tag.details}}
+        li
+          b time:&nbsp;
+          | {{tag.crop.time}}
+        li
+          b range:&nbsp;
+          | {{tag.range}}
 </template>
 
 <style scoped lang="sass">
   .tag-card
+    display: flex
     margin: 32px
     width: calc(100% - 64px)
     height: 200px
-  .media
-    height: 100%
-    width: 30vw
+  .image
+    height: 200px
+    width: 200px
+    object-fit: contain
     background-color: black
+    cursor: zoom-in
 </style>
