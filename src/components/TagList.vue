d46b32b7b7f5b2bbca3e20b2916447981627bf27
Ken Li <iliawnek@gmail.com>
2018-01-30 01:25:16 +0000

    Show and allow filtering of tag sub-type values in tag-list

--- src/components/TagList.vue
+++ src/components/TagList.vue
@@ -1,6 +1,7 @@
 <script>
 import {tagForm} from '@/values/tagValues'
 import TagForm from '@/components/TagForm'
+import {capitalise} from '@/utilities'
 
 export default {
   name: 'tag-list',
@@ -15,7 +16,7 @@ export default {
 
   data () {
     return {
-      filter: tagForm({includeDescription: true}),
+      filter: tagForm({}),
       isFilterDialogOpen: false,
     }
   },
@@ -25,9 +26,23 @@ export default {
       return this.tags && this.tags.length > 0
     },
     filteredTags () {
-      return this.tags && this.tags.filter((tag) => {
-        const typeFilter = this.filter.type ? tag.details.type === this.filter.type : true
-        return typeFilter
+      return this.tags && this.filter && this.tags.filter((tag) => {
+        if (this.filter.type) {
+          const doesTagMatchType = tag.details.type === this.filter.type
+          if (doesTagMatchType) {
+            // tag must match selected type
+            let filter = doesTagMatchType
+            // tag sub-type values must match each selected (non-null) filter value
+            Object.entries(this.filter[this.filter.type]).forEach(([subType, value]) => {
+              if (value) filter = filter && tag.details[subType] === value
+            })
+            return filter
+          } else {
+            return false // tag doesn't match selected type
+          }
+        } else {
+          return true // no type, therefore no filter
+        }
       })
     },
     totalCount () {
@@ -39,6 +54,31 @@ export default {
     tagCountText () {
       return `Showing ${this.filteredCount} of ${this.totalCount} ${this.totalCount === 1 ? 'tag' : 'tags'}`
     },
+    tagListHelpText () {
+      return this.filter.type
+        ? 'List is filtered. Remove filter to show all tags.'
+        : 'Filter by type for more detailed information.'
+    },
+  },
+
+  methods: {
+    clearForm () {
+      this.filter.type = null
+      this.filter.people.amount = null
+      this.filter.people.state = null
+      this.filter.people.freedom = null
+      this.filter.water.form = null
+      this.filter.vehicle.state = null
+      this.filter.area.form = null
+      this.filter.area.shelter = null
+      this.filter.area.service = null
+      this.filter.damage.form = null
+      this.filter.description = null
+    },
+
+    capitalise (string) {
+      return capitalise(string)
+    },
   },
 }
 </script>
@@ -46,8 +86,10 @@ export default {
 <template lang="pug">
   .tag-list
     .tag-list-header
-      .tag-list-title {{tagCountText}}
-      md-button.md-raised(@click="isFilterDialogOpen = true") Filter
+      div
+        .tag-list-title {{tagCountText}}
+        .tag-list-subtitle {{tagListHelpText}}
+      md-button.md-accent.md-raised(@click="isFilterDialogOpen = true") Filter
 
     md-dialog(
     :md-active.sync="isFilterDialogOpen"
@@ -58,14 +100,19 @@ export default {
       :clearable="true"
       )
       md-dialog-actions
-        md-button Clear all
+        md-button(@click="clearForm") Clear all
         md-button.md-primary(@click="isFilterDialogOpen = false") Done
 
     md-table(v-if="doTagsExist" md-card)
       md-table-row
-        md-table-head
+        md-table-head Play video
         md-table-head Image
         md-table-head Type
+        template(v-if="filter.type")
+          md-table-head(
+          v-for="subType in Object.keys(filter[filter.type])"
+          :key="subType"
+          ) {{capitalise(subType)}}
         md-table-head Description
       md-table-row(v-for="tag in filteredTags" :key="tag['.key']")
         md-table-cell
@@ -74,6 +121,11 @@ export default {
         md-table-cell
           img.thumbnail(:src="tag.crop.images.highlighted")
         md-table-cell {{tag.details.type}}
+        template(v-if="filter.type")
+          md-table-cell(
+          v-for="subType in Object.keys(filter[filter.type])"
+          :key="subType"
+          ) {{tag.details[subType]}}
         md-table-cell {{tag.details.description}}
 </template>
 
@@ -86,6 +138,8 @@ export default {
   .tag-list-title
     font-size: 1.2em
     font-weight: bold
+  .tag-list-subtitle
+    font-size: 0.9em
   .thumbnail
     height: 50px
     object-fit: cover
